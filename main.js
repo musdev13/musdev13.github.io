ad18a98b2e155721e2f3e65d69c9a1c04930988a
musdev13 <sonics0977703996@gmail.com>
2025-08-12 23:55:33 +0300

    done?

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ let rightBtn = document.getElementById('right');
 let canvas = document.querySelector('.canvas');
 
 const list = [
+    `<div class="intro">
+        <h1>Welcome</h1>
+        <h2>I guess I should introduce myself...</h2>
+    </div>`,
     `<div class="about">
         <img src="assets/img/mus.png" alt="">
         <div class="info">
@@ -14,31 +18,47 @@ const list = [
     `<div class="projects">
         <h1>Projects</h1>
         <div class="devider"></div>
-        <div class="row">
-            <div class="item">
-                <img src="https://raw.githubusercontent.com/musdev13/rain/main/docs/public/assets/mainScreenshot.png">
-                <div class="devider"></div>
-                <h2><img src="https://github.com/musdev13/rain/blob/main/docs/public/assets/favicon.png?raw=true">Rain</h2>
-                <div class="icons">
-                    <a href="#"><i class="fa-brands fa-github"></i></a>
-                    <a href="#"><i class="fa-solid fa-earth-americas"></i></a>
+        <div class="column">
+            <div class="row">
+                <div class="item">
+                    <img src="https://raw.githubusercontent.com/musdev13/rain/main/docs/public/assets/mainScreenshot.png">
+                    <div class="devider"></div>
+                    <h2><img src="https://github.com/musdev13/rain/blob/main/docs/public/assets/favicon.png?raw=true">Rain</h2>
+                    <div class="icons">
+                        <a target="_blank" href="https://github.com/musdev13/rain"><i class="fa-brands fa-github"></i></a>
+                        <a target="_blank" href="https://musdev13.github.io/rain/"><i class="fa-solid fa-earth-americas"></i></a>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="https://github.com/musdev13/TTS-to-Microphone/blob/main/TTStM.jpg?raw=true">
+                    <div class="devider"></div>
+                    <h2>TTStM</h2>
+                    <div class="icons">
+                        <a target="_blank" href="https://github.com/musdev13/TTS-to-Microphone"><i class="fa-brands fa-github"></i></a>
+                    </div>
                 </div>
             </div>
-            <div class="item">
-                <img src="https://github.com/musdev13/TTS-to-Microphone/blob/main/TTStM.jpg?raw=true">
-                <div class="devider"></div>
-                <h2>TTStM</h2>
-                <div class="icons">
-                    <a href="#"><i class="fa-brands fa-github"></i></a>
+            <div class="row">
+                <div class="item">
+                    <img src="https://github.com/musdev13/lane-adb/blob/main/2025-08-12-234557_hyprshot.png?raw=true">
+                    <div class="devider"></div>
+                    <h2>lane-adb</h2>
+                    <div class="icons">
+                        <a target="_blank" href="https://github.com/musdev13/lane-adb"><i class="fa-brands fa-github"></i></a>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="https://github.com/musdev13/lane-adb/blob/main/2025-08-12-234557_hyprshot.png?raw=true">
+                    <div class="devider"></div>
+                    <h2>lane-wd</h2>
+                    <div class="icons">
+                        <a target="_blank" href="https://github.com/musdev13/lane-wd"><i class="fa-brands fa-github"></i></a>
+                    </div>
                 </div>
             </div>
         </div>
     </div>`
 ];
-
-let slide = 0;
-canvas.innerHTML=list[0];
-leftBtn.disabled = true;
 
 function move(direction) {
     rightBtn.disabled = true;
@@ -73,6 +93,11 @@ function move(direction) {
         canvas.removeEventListener("transitionend", handler);
     });
 }
+
+let slide = 0;
+canvas.classList.remove("hidden")
+canvas.innerHTML=list[slide];
+leftBtn.disabled = true;
 
 rightBtn.addEventListener("click", () => move(true));
 leftBtn.addEventListener("click", () => move(false));
